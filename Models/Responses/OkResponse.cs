d9040c21d0a93d78a1dad2dd494cf6480f91a43d
Cristian Hernandez <chinorubikguitar@hotmail.com>
2024-09-22 19:46:33 -0600

    feature(login request,, handle simple response, username and email uniques)

---
+++ Models/Responses/OkResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace social_oc_api.Models.Responses
+{
+    public class OkResponse
+    {
+
+            public int Status { get; set; }
+            public string Message { get; set; }
+
+    }
+}
